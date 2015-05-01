6246a87cf926f6a2e044f9aee09d7505fe0666d8
Alexander K <knuxus@gmail.com>
2015-05-01 23:05:24 +0100

    codeship and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-   resources :users
+  resources :users
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
